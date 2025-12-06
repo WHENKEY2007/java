92c61733d53d8bcb7a473e0addeda0307eb061ce
N.Sri Venkatesh <srivenkateshnurukurthi@gmail.com>
2025-12-06 10:53:02 +0530

    update

--- Typecasting.java
+++ Typecasting.java
@@ -1,3 +1,11 @@
-public class Typecasting {
+class typecasting{
+    public static void main(String[] args){
+        int x=10;
+        double y=x;
+        System.out.println(y);
     
+        double a=12.423;
+        int b=(int)a;
+        System.out.println(b);
+    }
 }
