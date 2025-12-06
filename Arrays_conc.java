a62b05f25b117415b582204d17fd1601be25de7a
N.Sri Venkatesh <srivenkateshnurukurthi@gmail.com>
2025-12-06 11:33:10 +0530

    update

---
+++ Arrays_conc.java
@@ -0,0 +1,13 @@
+public class Arrays_conc {
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5};
+        String str[]={"java","c","python"};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        for(int i=0;i<str.length;i++){
+            System.out.print(str[i]+" ");
+        }
+    }
+}
