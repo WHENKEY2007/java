24917f26e8eb9d281f7198b41f1613afb6d8259e
N.Sri Venkatesh <srivenkateshnurukurthi@gmail.com>
2025-12-26 10:31:29 +0530

    update

--- ConstructorExample1.java
+++ ConstructorExample1.java
@@ -6,18 +6,31 @@ public class ConstructorExample1{
     ConstructorExample1(){
         System.out.println("Default Constructor");
     }
+    ConstructorExample1(int a){
+        id=a;
+    }
     ConstructorExample1(int a,String s){
         id=a;
         name=s;
     }
+    ConstructorExample1(int a,String s,int b){
+        id=a;
+        name=s;
+        age=b;
+    }
     void display(){
-        System.out.println("id: "+id+"Name: "+name+"Age: "+age);
+        System.out.println("id:"+id+" "+"Name:"+name+" "+"Age:"+age);
     }
 
     public static void main(String[] args){
         ConstructorExample1 obj1=new ConstructorExample1();
-        ConstructorExample1 obj2=new ConstructorExample1(1,"Suresh");
+        ConstructorExample1 obj2=new ConstructorExample1(1);
+        ConstructorExample1 obj3=new ConstructorExample1(2,"Ramesh");
+        ConstructorExample1 obj4=new ConstructorExample1(3,"Suresh",18);
+
         obj1.display();
         obj2.display();
+        obj3.display();
+        obj4.display();
     }
 }
